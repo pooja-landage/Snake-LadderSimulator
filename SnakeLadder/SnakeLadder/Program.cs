9a71b09c5e1c897293f982f60c21c9472ae36e5b
pooja-landage <poojalandage22@gmail.com>
2021-11-19 16:35:58 +0530

    [Pooja] : Added UC1-PlayerPosition

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("\t \t  ---------Welcome to snake and Ladder Game......");
+            int player1 = 0;
+        }
+    }
+
+
+}
+
